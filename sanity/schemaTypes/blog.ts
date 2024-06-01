da978fae173872594dd5ea055580914f537907fd
nauman434 <76839009+nauman434@users.noreply.github.com>
2024-06-01 23:59:05 +0500

    added blog

---
+++ sanity/schemaTypes/blog.ts
@@ -0,0 +1,44 @@
+export default {
+    name: 'blog',
+    type: 'document',
+    title: 'Blog',
+    fields: [
+        {
+            name: 'title',
+            type: 'string',
+            title: 'Title'
+        },
+        {
+            name: 'company',
+            type: 'string',
+            title: 'Company'
+        },
+        {
+            name: 'slug',
+            type: 'slug',
+            title: 'Slug',
+            options: {
+                source: 'company',
+            }
+        },
+        {
+            name: 'image',
+            type: 'image',
+            title: 'Logo',
+        },
+        {
+            name: 'smallDescription',
+            type: 'text',
+            title: 'Small Description',
+        },
+        {
+            name: 'content',
+            type: 'array',
+            title: 'Content',
+            of: [
+                { type: 'block' },
+            ]
+        },
+        
+    ]
+}
