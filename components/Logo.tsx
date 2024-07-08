a9534624bff760c52a7911a89cf58b602bd6770c
nauman434 <76839009+nauman434@users.noreply.github.com>
2024-07-08 12:19:19 +0500

    increased logo size

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
   return (
     <Link href={'/'}>
       <div className='h-[40px] flex items-center sm:justify-start justify-center  gap-6'>
-        <Image src={'/logo_black.svg'} width={120} height={120} alt='logo' className='hover:transition-all ease-in hover:scale-110 duration-300 cursor-pointer' />
+        <Image src={'/logo_black.svg'} width={150} height={150} alt='logo' className='hover:transition-all ease-in hover:scale-110 duration-300 cursor-pointer' />
       </div>
     </Link>
   )
